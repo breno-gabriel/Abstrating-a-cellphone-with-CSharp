92be73d395e72441dd274869fb56e0f5f7a1c930
Breno <brenomelogabriel2019@gmail.com>
2024-01-08 11:36:07 -0300

    adding the inhered constructor for 'Nokia'

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,19 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    public class Nokia: Smartphone
     {
+
+        public Nokia(string number, string model, string imei, int memory) : base( number, model, imei, memory)
+        {
+            
+        }
+
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            throw new NotImplementedException();
+        }
+
     }
 }
