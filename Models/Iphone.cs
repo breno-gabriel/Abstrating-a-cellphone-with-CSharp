9278a171670814adb57e7d0290acb8311a0afe8b
Breno <brenomelogabriel2019@gmail.com>
2024-01-08 13:23:14 -0300

    completing the Nokia class

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,20 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
+
+        public Iphone(string number, string model, string imei, int memory) : base(number, model, imei, memory)
+        {
+            
+        }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine("Acessando Apple store.....");
+            Console.WriteLine($"Instalando {nomeApp}");
+        }
+
         // TODO: Sobrescrever o método "InstalarAplicativo"
     }
 }
--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -4,7 +4,7 @@ namespace DesafioPOO.Models
     public class Nokia: Smartphone
     {
 
-        public Nokia(string number, string model, string imei, int memory) : base( number, model, imei, memory)
+        public Nokia(string number, string model, string imei, int memory) : base(number, model, imei, memory)
         {
             
         }
